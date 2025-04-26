8b9777df036faf85ea9d9fc47ddae1ac35d6066b
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-04-26 19:40:30 -0400

    updated student.js

--- database/models/Student.js
+++ database/models/Student.js
@@ -15,7 +15,27 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
+},
+
+email: {
+  type: Sequelize.STRING,
+  allowNull: false
+},
+
+imageUrl: {
+  type: Sequelize.STRING,
+  allowNull: true,
+  defaultValue: "/images/student.png"
+},
+
+gpa: {
+  type: Sequelize.FLOAT,
+  allowNull: true,
+  validate: {
+    min: 0.0,
+    max: 4.0
   }
+}
 });
 
 // Export the student model
