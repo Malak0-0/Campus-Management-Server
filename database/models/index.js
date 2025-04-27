7ed1b3f89b9386612fe94630416f5c965e658860
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-04-26 21:18:22 -0400

    minor fixes

--- database/models/index.js
+++ database/models/index.js
@@ -6,7 +6,7 @@ It registers models, sets up associations between tables, and generates barrel f
 const Student  = require('./Student');  // Import Student model
 const Campus  = require('./Campus');  // Import Campus model
 
-Student.belongsTo(Campus);  // Student belongs to only one Campus 
+Student.belongsTo(Campus,{ foreignKey: { allowNull: true } } );  // Student belongs to only one Campus 
 Campus.hasMany(Student);  // Campus can have many Student
 
 // Export models and associations
